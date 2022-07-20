7837961b9344274ea74f5c2f3abd1fbc699de684
Tobias Braner <t.braner@triggerco.de>
2022-07-20 17:29:04 +0200

    compiled

--- dist/utils.js
+++ dist/utils.js
@@ -27,7 +27,7 @@ const castFeathersQueryToPrismaFilters = (p, whitelist) => {
         if (prismaKey && key !== '$eager' && whitelist.includes(key)) {
             const value = p[key];
             if (Array.isArray(value)) {
-                filters[prismaKey] = value.map((v) => (0, exports.castToNumberBooleanStringOrNull)(v));
+                filters[prismaKey] = value.map((v) => exports.castToNumberBooleanStringOrNull(v));
             }
             else if (key === '$rawWhere' && typeof value === 'object') {
                 Object.keys(value).forEach((rawKey) => {
@@ -35,7 +35,7 @@ const castFeathersQueryToPrismaFilters = (p, whitelist) => {
                 });
             }
             else if (value !== undefined && typeof value !== 'object') {
-                filters[prismaKey] = (0, exports.castToNumberBooleanStringOrNull)(value);
+                filters[prismaKey] = exports.castToNumberBooleanStringOrNull(value);
             }
         }
     });
@@ -43,45 +43,46 @@ const castFeathersQueryToPrismaFilters = (p, whitelist) => {
 };
 exports.castFeathersQueryToPrismaFilters = castFeathersQueryToPrismaFilters;
 const castEagerQueryToPrismaInclude = (value, whitelist, idField) => {
-    const include = {};
-    if (Array.isArray(value)) {
-        value.forEach((v) => {
-            if (Array.isArray(v) && typeof v[0] === 'string' && v.length > 1) {
-                const [key, ...includes] = v;
-                const subinclude = (0, exports.castEagerQueryToPrismaInclude)(includes, whitelist, idField);
-                include[key] = {
-                    include: subinclude,
-                };
-            }
-            else if (Array.isArray(v) && typeof v[0] === 'string' && v.length === 1) {
-                const [key] = v;
-                include[key] = true;
-            }
-            else if (typeof v[0] !== 'string') {
-                throw {
-                    code: 'FP1001',
-                    message: 'First Array Item in a sub-array must be a string!',
-                };
-            }
-            else if (typeof v === 'string') {
-                include[v] = true;
-            }
-        });
-    }
-    else {
-        Object.keys(value).forEach((key) => {
-            const val = value[key];
-            if (typeof val === 'boolean') {
-                include[key] = val;
-            }
-            else if (Array.isArray(val)) {
-                include[key] = {
-                    select: Object.assign({ [idField]: true }, (0, exports.buildSelect)(val)),
-                };
-            }
-        });
-    }
-    return include;
+    // we don't care about feathers compliance, we want where queries in our include
+    // thus just returnung the $eager value as include 1:1
+    return value;
+    // const include: Record<string, any> = {};
+    // if (Array.isArray(value)) {
+    //   value.forEach((v) => {
+    //     if (Array.isArray(v) && typeof v[0] === 'string' && v.length > 1) {
+    //       const [key, ...includes] = v;
+    //       const subinclude = castEagerQueryToPrismaInclude(includes, whitelist, idField);
+    //       include[key] = {
+    //         include: subinclude,
+    //       };
+    //     } else if (Array.isArray(v) && typeof v[0] === 'string' && v.length === 1) {
+    //       const [key] = v;
+    //       include[key] = true;
+    //     } else if (typeof v[0] !== 'string') {
+    //       throw {
+    //         code: 'FP1001',
+    //         message: 'First Array Item in a sub-array must be a string!',
+    //       };
+    //     } else if (typeof v === 'string') {
+    //       include[v] = true;
+    //     }
+    //   });
+    // } else {
+    //   Object.keys(value).forEach((key) => {
+    //     const val = value[key];
+    //     if (typeof val === 'boolean') {
+    //       include[key] = val;
+    //     } else if (Array.isArray(val)) {
+    //       include[key] = {
+    //         select: {
+    //           [idField]: true,
+    //           ...buildSelect(val),
+    //         },
+    //       };
+    //     }
+    //   });
+    // }
+    // return include;
 };
 exports.castEagerQueryToPrismaInclude = castEagerQueryToPrismaInclude;
 const mergeFiltersWithSameKey = (where, key, filter) => {
@@ -99,25 +100,25 @@ const buildWhereAndInclude = (query, whitelist, idField) => {
     Object.keys(query).forEach((k) => {
         const value = query[k];
         if (k === '$or' && Array.isArray(value)) {
-            where.OR = value.map((v) => (0, exports.buildWhereAndInclude)(v, whitelist, idField).where);
+            where.OR = value.map((v) => exports.buildWhereAndInclude(v, whitelist, idField).where);
         }
         else if (k === '$and' && Array.isArray(value)) {
             value.forEach((v) => {
-                const whereValue = (0, exports.buildWhereAndInclude)(v, whitelist, idField).where;
+                const whereValue = exports.buildWhereAndInclude(v, whitelist, idField).where;
                 Object.keys(whereValue).map((subKey) => {
-                    where[subKey] = (0, exports.mergeFiltersWithSameKey)(where, subKey, whereValue[subKey]);
+                    where[subKey] = exports.mergeFiltersWithSameKey(where, subKey, whereValue[subKey]);
                 });
             });
         }
         else if (k !== '$eager' && typeof value === 'object' && !Array.isArray(value)) {
-            where[k] = (0, exports.mergeFiltersWithSameKey)(where, k, (0, exports.castFeathersQueryToPrismaFilters)(value, whitelist));
+            where[k] = exports.mergeFiltersWithSameKey(where, k, exports.castFeathersQueryToPrismaFilters(value, whitelist));
         }
         else if (k !== '$eager' && typeof value !== 'object' && !Array.isArray(value)) {
-            where[k] = (0, exports.castToNumberBooleanStringOrNull)(value);
+            where[k] = exports.castToNumberBooleanStringOrNull(value);
         }
         else if (k === '$eager' && whitelist.includes(k)) {
             const eager = value;
-            include = (0, exports.castEagerQueryToPrismaInclude)(eager, whitelist, idField);
+            include = exports.castEagerQueryToPrismaInclude(eager, whitelist, idField);
         }
     });
     return { where, include };
@@ -141,12 +142,12 @@ const buildPagination = ($skip, $limit) => {
 };
 exports.buildPagination = buildPagination;
 const buildPrismaQueryParams = ({ id, query, filters, whitelist }, idField) => {
-    let select = (0, exports.buildSelect)(filters.$select || []);
+    let select = exports.buildSelect(filters.$select || []);
     const selectExists = Object.keys(select).length > 0;
-    const { where, include } = (0, exports.buildWhereAndInclude)(id ? Object.assign({ [idField]: id }, query) : query, whitelist, idField);
+    const { where, include } = exports.buildWhereAndInclude(id ? Object.assign({ [idField]: id }, query) : query, whitelist, idField);
     const includeExists = Object.keys(include).length > 0;
-    const orderBy = (0, exports.buildOrderBy)(filters.$sort || {});
-    const { skip, take } = (0, exports.buildPagination)(filters.$skip, filters.$limit);
+    const orderBy = exports.buildOrderBy(filters.$sort || {});
+    const { skip, take } = exports.buildPagination(filters.$skip, filters.$limit);
     const queryWhereExists = Object.keys(where).filter((k) => k !== idField).length > 0;
     const idQueryIsObject = typeof where.id === 'object';
     if (selectExists) {
